2fad15814c858a930f5bda2e4202f70c8646658a
pedrobueni <00001218781154sp@aluno.educacao.sp.gov.br>
2024-11-27 12:26:30 -0300

    fasch card pedro

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Influencer',
+    'Sucessos atuais de SrPedroBueno',
+    'Hellow tudo bem? atualmente o digital influencer SrPedrobueo das multi plataformas de twitch tv atual plataforma que ele trabalha ! e tem as demais midias que ele posta os clipes das lives do melhores momentos !'
+)
+
+criaCartao(
+    'Redes Sociais',
+    'Qual a redes sociais dele?',
+    'Twitch.tv:SrPedroBueno-instagram:SrPedroBueno-X: Hypejocker33-tiktoksrpedrobueno'
+ 
+)
+
+criaCartao(
+'Familia',
+'Quantos irmãos(a) Bueno tem?',
+'Bueno tem atualmente 11 irmãos(a) 5 mulheres e 2 adotivos do primeiro casamento e do segundo casamento em si de mãe bueno teve 5 filhos(a) e bueno sendo o caçula e ela teve 4 mulheres e uma filha transsexual que se assumiu recentemente !'
+)
+
+criaCartao(
+    'Atuação',
+    'Qual seu atual hoobs?',
+    'Bueno sempre houve seu interesse pela tecnologia desde criança e sempre gostou de mexer com programação desde seus 7 anos de idade ele começou a demostrar em interesse em fazer lives streaming e decidiu virar um digital influencer em 2017 então ele tem 7 anos de carreira como enfluencer e divulgador de servidores!'
+)
